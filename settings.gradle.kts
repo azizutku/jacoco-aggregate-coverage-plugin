3c97fa41b68f93ea42da011aa8cb92da66550d04
Aziz Utku Kagitci <azizutku@gmail.com>
2023-11-01 19:41:27 +0300

    Set up initial project

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.google.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Jacoco Aggregate Coverage Plugin"
+include(":jacocoaggregatecoverageplugin")
